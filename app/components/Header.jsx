c8efac4fd3b507e0cf62ed818fb07b9b9847d195
David <juanlc1526@gmail.com>
2023-11-18 01:25:24 -0500

    quiero dormir

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,15 +3,18 @@ import { auth, UserButton } from "@clerk/nextjs";
 import BurguerMenu from "./BurguerMenu";
 
 const Header = () => {
-
+  const { userId } = auth()
 
   return (
     <>
       <nav className="bg-lightGray w-full h-[48px] fixed top-0 z-50">
+        <BurguerMenu isLogged={userId === null} />
       </nav>
+      {userId && (
         <div className="bg-lightGray w-[20vw] h-[48px] fixed top-0 right-0 z-50 p-2">
+          <UserButton afterSignOutUrl="/" />
         </div>
-
+      )}
     </>
   );
 };
