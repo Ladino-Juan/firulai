8b226bd232494d7763429320c97d1c001bdfc9d9
David <juanlc1526@gmail.com>
2023-11-18 18:22:58 -0500

    clerk middleware test

--- middleware.js
+++ middleware.js
@@ -1,6 +1,19 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
  
 export default authMiddleware({
+  afterAuth(auth, req) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      if (
+        req.nextUrl.pathname === "/sign-in" ||
+        req.nextUrl.pathname === "/sign-up"
+      ) {
+        const url = new URL("/", req.url);
+        return NextResponse.redirect(url);
+      }
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
     publicRoutes: ['/']
 });
  
