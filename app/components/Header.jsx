1866d9cbc2cd574b8c7fa99b56e56a8523244adf
David <juanlc1526@gmail.com>
2023-11-18 00:47:42 -0500

    bad debugging

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,24 +3,15 @@ import { auth, UserButton } from "@clerk/nextjs";
 import BurguerMenu from "./BurguerMenu";
 
 const Header = () => {
-  let userId = null;
-  try {
-    const authInfo = auth();
-    userId = authInfo.userId;
-  } catch (error) {
-    console.error('Error fetching user authentication:', error);
-  }
+
 
   return (
     <>
       <nav className="bg-lightGray w-full h-[48px] fixed top-0 z-50">
-        <BurguerMenu isLogged={userId === null} />
       </nav>
-      {userId && (
         <div className="bg-lightGray w-[20vw] h-[48px] fixed top-0 right-0 z-50 p-2">
-          <UserButton afterSignOutUrl="/" />
         </div>
-      )}
+
     </>
   );
 };
