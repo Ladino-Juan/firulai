770a08b71ba4206c1122f8ca95edaf8f9669ae52
David <juanlc1526@gmail.com>
2023-11-25 00:05:09 -0500

    again the canvas problem

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -2,9 +2,21 @@
 
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useState, useEffect, Suspense } from "react";
 import { Rocky } from "./Rocky";
 
 const Experience = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay for demonstration purposes
+    const timer = setTimeout(() => {
+      setIsLoading(false); // Set isLoading to false after a delay (you can replace this with your logic)
+    }, 2000); // Change the delay time as needed
+
+    // Clear timeout if component unmounts
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
       <Canvas camera={{ position: [5, 1.5, 8], fov: 50 }} shadows>
@@ -17,8 +29,10 @@ const Experience = () => {
           shadow-mapSize-height={1024}
         />
         <group position={[0, -1, 0]}>
-
-          <Rocky />
+          <Suspense fallback={null}>
+            {!isLoading && <Rocky />}{" "}
+            {/* Render Rocky component when isLoading is false */}
+          </Suspense>
         </group>
       </Canvas>
     </>
