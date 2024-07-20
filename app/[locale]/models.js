63d165ad6b9ed7bc583d276c209d2cc0f9fba9d7
David <juanlc1526@gmail.com>
2024-07-19 22:53:18 -0500

    v.1.0.10

--- app/[locale]/models.js
+++ app/[locale]/models.js
@@ -1,13 +1,13 @@
 export const dogs = [
   {
-    id: "rec_cm2ven37qb0vcnhgaitg",
-    url: "https://eu-west-1.storage.xata.sh/jhcp1da7gl66h5ujjjp50ur6q0",
+    id: "rec_cmfcb4nk9huknrvjlcng",
+    url: "https://eu-west-1.storage.xata.sh/q5jf077f957gf9ktoh9fbth2dg",
     firuData: {
-      name: "Paris",
+      name: "Tafy",
       history:
-        "Paris, una perrita valiente y amorosa, enfrentó un desafío desde temprana edad: perdió una de sus patitas tras ser atropellada por una buseta en Villa Santana a los 3 meses de edad. A pesar de su adversidad, Paris irradiaba alegría con su dulce mirada y su lealtad inquebrantable hacia quienes la rodeaban. Su espíritu resiliente inspiraba a todos quienes la conocían, recordándoles la fuerza del amor y la superación",
-      realFiru: "/assets/firus/firu2.webp",
-      modelFiru: "/assets/firus/firuModel2.webp",
+        "cuando apenas tenía dos días de nacida, estuvo a punto de ser arrojada al río, enfrentando una existencia incierta desde el inicio. Sin embargo, su historia tomó un giro esperanzador cuando alguien intervino para salvarla. A pesar de los desafíos que enfrentó en sus primeros momento. Su valentía y determinación nos recuerdan la importancia de la compasión y la protección hacia los más vulnerables de nuestra sociedad. Tafi merece un hogar lleno de amor y cuidados rodeada de afecto y seguridad",
+      realFiru: "/assets/firus/firu5.webp",
+      modelFiru: "/assets/firus/firuModel5.webp",
     },
   },
   {
@@ -16,7 +16,7 @@ export const dogs = [
     firuData: {
       name: "La abuela",
       history:
-        "La Abuela, una perrita de 8 años, ha llegado a la dulce etapa de la ancianidad con gracia y gentileza. Su corazón rebosa de amor y su dulzura ilumina cualquier espacio que pisa. Con años de sabiduría y experiencias compartidas, la Abuela es un faro de bondad, irradiando calidez y amor a todos los que tienen el privilegio de conocerla. Su historia es un recordatorio conmovedor de la belleza y el valor de cada etapa de la vida, enseñándonos que la verdadera riqueza reside en el amor y la amabilidad que compartimos con los demás.",
+        "La Abuela, una perrita de 8 años, ha llegado a la dulce etapa de la ancianidad con gracia. Su corazón rebosa de amor y su dulzura ilumina cualquier espacio que pisa. Con años de sabiduría y experiencias compartidas, la Abuela es un faro de bondad, irradiando calidez y amor a todos los que tienen el privilegio de conocerla. Su historia es un recordatorio conmovedor de la belleza y el valor de los animales",
       realFiru: "/assets/firus/firu1.webp",
       modelFiru: "/assets/firus/firuModel1.webp",
     },
@@ -27,7 +27,7 @@ export const dogs = [
     firuData: {
       name: "Amelia",
       history:
-        "Amelia, una perrita valiente y adorable, enfrentó una experiencia desafiante a los tres meses de edad cuando un encuentro con un gato le arrebató uno de sus preciosos ojitos. A pesar de la adversidad, su espíritu no se quebrantó. Con un solo ojito, Amelia irradia una ternura incomparable y una determinación inquebrantable. Su historia es un recordatorio conmovedor del poder de la resiliencia y el amor incondicional, inspirando a todos aquellos que tienen el privilegio de cruzarse en su camino.",
+        "Amelia, una perrita valiente y adorable, enfrentó una experiencia desafiante a los tres meses de edad cuando un encuentro con un gato le arrebató uno de sus preciosos ojitos. A pesar de la adversidad, su espíritu no se quebrantó. Con un solo ojito, Amelia irradia una ternura incomparable y una determinación inquebrantable, inspirando a todos aquellos que tienen el privilegio de cruzarse en su camino.",
       realFiru: "/assets/firus/firu3.webp",
       modelFiru: "/assets/firus/firuModel3.webp",
     },
@@ -38,26 +38,26 @@ export const dogs = [
     firuData: {
       name: "Koral",
       history:
-        "Koral, una pitbull amorosa, fue abandonada en Villa Verde, llevando consigo el peso del abandono y la pérdida. A pesar del dolor de perder a la mitad de sus cachorros, quienes fueron arrojados al río, su corazón sigue siendo un manantial de amor incondicional. Su historia es un recordatorio conmovedor de la resiliencia de los lazos familiares y la fuerza del amor maternal. Koral merece un hogar donde pueda sanar sus heridas emocionales y ser apreciada por la lealtad y el cariño que ofrece sin reservas.",
+        "Koral, una pitbull amorosa, fue abandonada en Villa Verde, llevando consigo el peso del abandono y la pérdida. A pesar del dolor de perder a la mitad de sus cachorros, quienes fueron arrojados al río, su corazón sigue siendo un manantial de amor incondicional. Koral merece un hogar donde pueda sanar sus heridas emocionales y ser apreciada por la lealtad y el cariño que ofrece sin reservas.",
       realFiru: "/assets/firus/firu4.webp",
       modelFiru: "/assets/firus/firuModel4.webp",
     },
   },
   {
-    id: "rec_cmfcb4nk9huknrvjlcng",
-    url: "https://eu-west-1.storage.xata.sh/q5jf077f957gf9ktoh9fbth2dg",
+    id: "rec_cm2ven37qb0vcnhgaitg",
+    url: "https://eu-west-1.storage.xata.sh/jhcp1da7gl66h5ujjjp50ur6q0",
     firuData: {
-      name: "Tafy",
+      name: "Paris",
       history:
-        "cuando apenas tenía dos días de nacida, estuvo a punto de ser arrojada al río, enfrentando una existencia incierta desde el inicio. Sin embargo, su historia tomó un giro esperanzador cuando alguien intervino para salvarla. A pesar de los desafíos que enfrentó en sus primeros momentos, Tafi es un símbolo de resiliencia y supervivencia. Su valentía y determinación nos recuerdan la importancia de la compasión y la protección hacia los más vulnerables de nuestra sociedad. Tafi merece un hogar lleno de amor y cuidados, donde pueda florecer y prosperar rodeada de afecto y seguridad",
-      realFiru: "/assets/firus/firu5.webp",
-      modelFiru: "/assets/firus/firuModel5.webp",
+        "Paris, una perrita valiente y amorosa, enfrentó un desafío desde temprana edad, perdió una de sus patitas tras ser atropellada a los 3 meses de edad. A pesar de su adversidad, Paris irradia alegría con su dulce mirada y su lealtad inquebrantable hacia quienes la rodean",
+      realFiru: "/assets/firus/firu2.webp",
+      modelFiru: "/assets/firus/firuModel2.webp",
     },
   },
   {
     id: "rec_cmfcb5kq7ahacfdtc8c0",
     url: "https://eu-west-1.storage.xata.sh/r4o8p86qf50mdf6tpi33spdlfo",
-    firuData: {name:"Rocky",history:"Rocky, un perro café de tamaño mediano, fue dejado atrás cuando su familia decidió partir al extranjero, sin encontrar a nadie dispuesto a cuidarlo. A pesar de su situación, Rocky es un torbellino de energía y alegría, siempre buscando amor y atención. Su naturaleza juguetona y bulliciosa lo hace único, llenando cada día con travesuras y risas. Aunque su historia refleja el abandono, su espíritu juguetón y su corazón leal siguen buscando un hogar donde su vitalidad sea apreciada y celebrada.",  realFiru: "/assets/firus/firu6.webp",
+    firuData: {name:"Rocky",history:"Rocky fue dejado atrás cuando su familia decidió partir al extranjero, sin encontrar a nadie dispuesto a cuidarlo. A pesar de su situación, Rocky es un torbellino de energía y alegría, siempre buscando amor y atención. Su naturaleza juguetona y bulliciosa lo hace único, llenando cada día con travesuras y risas. Aunque su historia refleja el abandono, su espíritu juguetón y su corazón leal siguen buscando un hogar donde su vitalidad sea apreciada y celebrada.",  realFiru: "/assets/firus/firu6.webp",
     modelFiru: "/assets/firus/firuModel6.webp",
     },
   },
