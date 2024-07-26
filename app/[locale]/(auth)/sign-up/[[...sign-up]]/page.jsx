db86d0c4de05ba97b2816cd65367dec51f997ae8
David <juanlc1526@gmail.com>
2024-07-26 05:11:53 -0500

    v1.1.4

--- app/[locale]/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/[locale]/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -2,7 +2,9 @@ import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => {
   return (
+    <div className="w-full h-[120vh] flex justify-center items-center">
       <SignUp />
+      </div>
   );
 };
 
