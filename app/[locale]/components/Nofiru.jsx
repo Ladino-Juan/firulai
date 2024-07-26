dfaa1d497c837a72659ca400ca9e1d405f503b26
David <juanlc1526@gmail.com>
2024-07-26 05:03:12 -0500

    v1.1.3

--- app/[locale]/components/Nofiru.jsx
+++ app/[locale]/components/Nofiru.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Nofiru = () => {
   return (
-    <div className='flex flex-col w-[90vw] h-[90vh] items-center justify-center space-y-5'>
+    <div className='flex flex-col w-full h-[90vh] items-center justify-center space-y-5'>
         <h1 className='font-bold text-6xl text-center text-gray-700'>APADRINA UN <span className='text-green-500'>FIRU</span></h1>
         <Link
             href={`/api/pets`}
